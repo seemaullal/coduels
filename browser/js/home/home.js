c3b45c487875a78ff95bcc9ddd8d710827a17b7a
Seema <seemaullal@gmail.com>
2015-03-18 10:11:15 -0400

    initial application from generator

---
+++ browser/js/home/home.js
@@ -0,0 +1,7 @@
+'use strict';
+app.config(function ($stateProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: 'js/home/home.html'
+    });
+});
---
+++ server/io/index.js
@@ -0,0 +1,15 @@
+'use strict';
+var socketio = require('socket.io');
+var io = null;
+
+module.exports = function (server) {
+
+    if (io) return io;
+
+    io = socketio(server);
+
+    io.on('connection', function (socket) {
+        // Now have access to socket, wowzers!
+    });
+
+};
