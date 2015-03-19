44f783f48ac1c7b62916c8e9ce251d9df436967e
Seema <seemaullal@gmail.com>
2015-03-19 09:34:36 -0400

    Changed about page

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -12,14 +12,6 @@ app.config(function ($stateProvider) {
 
 app.controller('AboutController', function ($scope) {
 
-    // Images of beautiful Fullstack people.
-    $scope.images = [
-        'https://pbs.twimg.com/media/B7gBXulCAAAXQcE.jpg:large',
-        'https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xap1/t31.0-8/10862451_10205622990359241_8027168843312841137_o.jpg',
-        'https://pbs.twimg.com/media/B-LKUshIgAEy9SK.jpg',
-        'https://pbs.twimg.com/media/B79-X7oCMAAkw7y.jpg',
-        'https://pbs.twimg.com/media/B-Uj9COIIAIFAh0.jpg:large',
-        'https://pbs.twimg.com/media/B6yIyFiCEAAql12.jpg:large'
-    ];
+  
 
 });
