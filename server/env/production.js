a48eb66639aca1ec972c62862f14b9a2b6a1127e
Linda Kung <lindakung417@gmail.com>
2015-03-20 18:15:52 -0400

    changed github callback variable name for deployment

--- server/env/production.js
+++ server/env/production.js
@@ -16,4 +16,9 @@ module.exports = {
         "clientSecret": process.env.GOOGLE_CLIENT_SECRET,
         "callbackURL": process.env.CALLBACK_URL
     }
+    "GITHUB": {
+        "clientID": process.env.GITHUB_CLIENT_ID,
+        "clientSecret": process.env.GITHUB_CLIENT_SECRET,
+        "callbackURL": process.env.GITHUB_CALLBACK_URL
+    }
 };
