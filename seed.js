1339ebf6da8e7f68fad91cddb831fcbdab8c126d
Linda Kung <lindakung417@gmail.com>
2015-03-23 16:56:00 -0400

    seed database

--- seed.js
+++ seed.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose');
 var async = require('async');
 require('./server/db/models/exercise.js');
-var Exercise = mongoose.model("Exercise")
+var Exercise = mongoose.model("Exercise");
+mongoose.connect("mongodb://coduels:javascript15@ds031641.mongolab.com:31641/coduels").connection;
 
 var data = {
 	Exercises : [
@@ -11,55 +12,8 @@ var data = {
 			name: 'Pig Latin',
 			shortDescription: 'Translate English phrases into Pig Latin', 
 			longDescription: 'Write a program that translates English to Pig Latin. Pig Latin has a few simple rules- Rule 1: If a word begins with a vowel sound, add "ay" to the end of the word. Rule 2: If a word begins with a consonant sound, move it to the end of the word, and then add an "ay" sound to the end of the word. There are a few more rules for edge cases; you can see http://en.wikipedia.org/wiki/Pig_latin for details.',
-			editorPrompt: 'function translateToPigLatin(englishText) {\n\n}';
-			testCode: "describe('pigLatin', function () {
-
-				  it('translates a word beginning with a', function () {
-				    expect(pigLatin.translate('apple')).toEqual('appleay');
-				  });
-
-				  it('translates a word beginning with e', function () {
-				    expect(pigLatin.translate('ear')).toEqual('earay');
-				  });
-
-				  it('translates a word beginning with p', function () {
-				    expect(pigLatin.translate('pig')).toEqual('igpay');
-				  });
-
-				  it('translates a word beginning with k', function () {
-				    expect(pigLatin.translate('koala')).toEqual('oalakay');
-				  });
-
-				  it('translates a word beginning with ch', function () {
-				    expect(pigLatin.translate('chair')).toEqual('airchay');
-				  });
-
-				  it('translates a word beginning with qu', function () {
-				    expect(pigLatin.translate('queen')).toEqual('eenquay');
-				  });
-
-				  it('translates a word with a consonant preceding qu', function () {
-				    expect(pigLatin.translate('square')).toEqual('aresquay');
-				  });
-
-				  it('translates a word beginning with th', function () {
-				    expect(pigLatin.translate('therapy')).toEqual('erapythay');
-				  });
-
-				  it('translates a word beginning with thr', function () {
-				    expect(pigLatin.translate('thrush')).toEqual('ushthray');
-				  });
-
-				  it('translates a word beginning with sch', function () {
-				    expect(pigLatin.translate('school')).toEqual('oolschay');
-				  });
-
-				  it('translates a phrase', function () {
-				    expect(pigLatin.translate('quick fast run'))
-				      .toEqual('ickquay astfay unray');
-				  });
-
-				});" ,
+			editorPrompt: 'function translateToPigLatin(englishText) {\n\n}',
+			testCode: "describe('pigLatin', function () {\nit('translates a word beginning with a', function () {\nexpect(translateToPigLatin('apple')).toEqual('appleay');\n});\nit('translates a word beginning with e', function () {  \nexpect(translateToPigLatin('ear')).toEqual('earay');\n});\nit('translates a word beginning with p', function () {\nexpect(translateToPigLatin('pig')).toEqual('igpay');\n});\nit('translates a word beginning with k', function () {\nexpect(translateToPigLatin('koala')).toEqual('oalakay');\n});\nit('translates a word beginning with ch', function () {\nexpect(translateToPigLatin('chair')).toEqual('airchay');\n});\nit('translates a word beginning with th', function () {  expect(translateToPigLatin('therapy')).toEqual('erapythay');\n});\nit('translates a word beginning with thr', function () {  expect(translateToPigLatin('thrush')).toEqual('ushthray');\n});\nit('translates a word beginning with sch', function () {\nexpect(translateToPigLatin('school')).toEqual('oolschay');\n});\n});\n",
 			category: ['functions','regex'],
 			difficulty: 'Medium'
 		},
@@ -78,8 +32,10 @@ var data = {
 mongoose.connection.on('open', function() {
 	mongoose.connection.db.dropDatabase(function() {
 		console.log('Adding seed data');
-		data.Exercises.forEach(function(exercise) {
-			Exercise.create()
+		async.each(data.Exercises, function(exercise, done) {
+			Exercise.create(exercise, done)
+		}, function(err) {
+			console.log('ctrl-c to exit');
 		})
 	})
 })
