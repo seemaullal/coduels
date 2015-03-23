5144a393aefd8bfa2cae90fb46f9baa22dc632d5
Michael Bae <michaelbbae@gmail.com>
2015-03-23 12:30:15 -0400

    ui-ace ng-model change

--- browser/js/arena/arena.js
+++ browser/js/arena/arena.js
@@ -13,13 +13,13 @@ app.controller('ArenaController', function($scope, RoomFactory, AuthService) {
   // in the newly created room
   AuthService.getLoggedInUser().then(function(user) {
     $scope.user = user;
-    $scope.test = RoomFactory.createRoom('exercise', $scope.user);
+    $scope.roomKey = RoomFactory.createRoom('exercise', $scope.user);
   });
 
   // defines and sets the onLoad callback function on the scope
   $scope.userInputSession = function(_editor) {
-    $scope.aceSession3 = _editor.getSession();
-    console.log($scope.aceSession3);
+    $scope.aceEditor = _editor.getSession();
+    console.log($scope.aceEditor);
   };
 
   // defines and sets the onChange callback function on the scope
@@ -39,7 +39,7 @@ app.controller('ArenaController', function($scope, RoomFactory, AuthService) {
           // if the enter key is pressed, we set the value of the userPad,
           // the JSON object at the url, to the value of whatever is in the
           // editor after enter key is pressed
-          userPad.set($scope.aceSession3.getDocument().getValue());
+          userPad.set($scope.aceEditor.getDocument().getValue());
 
           // the next line refreshes the iframe but I'm not sure this is entirely necessary
           // I believe I put this in initially because every time the test runs, it appends
@@ -51,4 +51,6 @@ app.controller('ArenaController', function($scope, RoomFactory, AuthService) {
       }); // closes keyup function
     }); // closes document.ready
   }; // closes getUserInput
+
+  $scope.userInputCode = 'function add(one, two) {\n\n}';
 }); // closes controller
