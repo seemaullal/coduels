f315a0f90bfebe7866d6c77f3a37744e8e6fe14e
Seema <seemaullal@gmail.com>
2015-03-23 20:45:51 -0400

    Fixed seed file

--- seed.js
+++ seed.js
@@ -12,7 +12,7 @@ var data = {
 			name: 'Pig Latin',
 			shortDescription: 'Translate English phrases into Pig Latin', 
 			longDescription: 'Write a program that translates English to Pig Latin. Pig Latin has a few simple rules- Rule 1: If a word begins with a vowel sound, add "ay" to the end of the word. Rule 2: If a word begins with a consonant sound, move it to the end of the word, and then add an "ay" sound to the end of the word. There are a few more rules for edge cases; you can see http://en.wikipedia.org/wiki/Pig_latin for details.',
-			editorPrompt: 'function translateToPigLatin(englishText) {\n\n}';
+			editorPrompt: 'function translateToPigLatin(englishText) {\n\n}',
 			testCode: "describe('pigLatin', function () {\nit('translates a word beginning with a', function () {\nexpect(translateToPigLatin('apple')).to.equal('appleay');\n});\nit('translates a word beginning with e', function () {  \nexpect(translateToPigLatin('ear')).to.equal('earay');\n});\nit('translates a word beginning with p', function () {\nexpect(translateToPigLatin('pig')).to.equal('igpay');\n});\nit('translates a word beginning with k', function () {\nexpect(translateToPigLatin('koala')).to.equal('oalakay');\n});\nit('translates a word beginning with ch', function () {\nexpect(translateToPigLatin('chair')).to.equal('airchay');\n});\nit('translates a word beginning with th', function () {  expect(translateToPigLatin('therapy')).to.equal('erapythay');\n});\nit('translates a word beginning with thr', function () {  expect(translateToPigLatin('thrush')).to.equal('ushthray');\n});\nit('translates a word beginning with sch', function () {\nexpect(translateToPigLatin('school')).to.equal('oolschay');\n});\n});\n" ,
 			category: ['functions','regex'],
 			difficulty: 'Medium'
