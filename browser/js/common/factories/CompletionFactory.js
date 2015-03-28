45b4da5f1f4ade83ac4bc2b958e5c3f88fdfb5a0
Seema <seemaullal@gmail.com>
2015-03-28 11:38:10 -0400

    Changed Completion Factory so that if numUsers = 1, isChallenge is set to false

--- browser/js/common/factories/CompletionFactory.js
+++ browser/js/common/factories/CompletionFactory.js
@@ -19,12 +19,17 @@ app.factory('CompletionFactory', function ($http){
 
 	factory.sendCompletion = function (userID, exerciseID, code, difficulty, numUsers, isWinner){
 		var score = factory.calculateScore(difficulty, numUsers);
+		var isChallenge = true;
+		if (numUsers === 1) 
+			isChallenge=false;
 		var completionObj = {
 			exerciseID: exerciseID, 
 			code: code, 
 			score: score, 
-			challenge: true
+			challenge: isChallenge
 		};
+		console.log('numUsers', numUsers);
+		console.log('completionObj',completionObj);
 		return $http.post('/api/users/'+ userID + '/exercises', completionObj).then(function(response){
 			return response.data;
 		});
