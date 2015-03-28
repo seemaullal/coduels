b78065e125708da953ffd317bdcef2ccba808c78
Seema <seemaullal@gmail.com>
2015-03-28 17:27:51 -0400

    Fixed merge conflict in server/io/index.js

--- server/io/index.js
+++ server/io/index.js
@@ -15,17 +15,11 @@ module.exports = function (server) {
         });
 
         socket.on('failures', function (failures){
-<<<<<<< HEAD
-            console.log('failures', failures);
             socket.broadcast.emit('theFailures', failures);
         });
 
         socket.on('testsFailed', function (arrFromIframe){
         	socket.broadcast.emit('failedTests', arrFromIframe);
-=======
-        	console.log('the failures in index.js', failures);
-        	socket.broadcast.emit('theFailures', failures);
->>>>>>> 1c0fe5ebf348846fd82bede7d9eee29b9ec08f25
         });
     });
 };
