0d5eeb5b5b4fcabb71b01367dc3971bfe7d6f540
Seema <seemaullal@gmail.com>
2015-03-28 23:38:55 -0400

    Added function to UsersFactory to retrieve most up to date verion of a user. Used this on Profile page to show updated tests taken before refresh

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -10,30 +10,27 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('AboutController', function ($scope, AuthService, TestFactory) {
+app.controller('AboutController', function ($scope, AuthService, TestFactory, UsersFactory) {
 
-	AuthService.getLoggedInUser().then(function(user){
-        console.log('userinfo', user);
-        $scope.user = user
+    AuthService.getLoggedInUser().then( function(archivedUser){
+        UsersFactory.getUser(archivedUser._id).then( function(user) {
+            $scope.user = user;
 
-        TestFactory.getExercises().then(function(data) {
-            $scope.userChallenges = [];
-            var exercisesInfo = _.pluck(data, "_id");
+            TestFactory.getExercises().then(function(data) {
+                $scope.userChallenges = [];
+                var exercisesInfo = _.pluck(data, "_id");
 
-            $scope.user.uniqueChallenges.forEach(function(challenge) {
-                var userChallenges = {};
-                var index = exercisesInfo.indexOf(challenge);
-                if(index == -1) {
-                    console.log('there are no unique challenges')
-                } else {
-                    userChallenges = data[index];
-                    $scope.userChallenges.push(userChallenges);
-                }
-            })
-
-            console.log('exercisesinfo please', $scope.userChallenges);
-        })
-    })
-
-
+                $scope.user.uniqueChallenges.forEach(function(challenge) {
+                    var userChallenges = {};
+                    var index = exercisesInfo.indexOf(challenge);
+                    if(index === -1) {
+                        console.log('there are no unique challenges');
+                    } else {
+                        userChallenges = data[index];
+                        $scope.userChallenges.push(userChallenges);
+                    }
+                });
+            });
+        });   
+    });
 });
