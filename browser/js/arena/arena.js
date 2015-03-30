2c58004a1f1931a88dc019ce5e033bf3fd3f2ddd
Seema <seemaullal@gmail.com>
2015-03-30 13:25:11 -0400

    Active rooms show up without refreshing"

--- browser/js/arena/arena.js
+++ browser/js/arena/arena.js
@@ -38,8 +38,7 @@ app.controller('ArenaController', function($scope, $stateParams, $sce, RoomFacto
           // $state.go('exercises');
           clearInterval(timeout);
           AuthService.getLoggedInUser().then(function(user) {
-            if (!isPractice)
-              user.isAuthorized = null;
+            user.isAuthorized = null;
             $scope.waitingDone = true;
             if ($scope.userDisplay.length === 1) {
               /*even if a user joined a challenge, if
