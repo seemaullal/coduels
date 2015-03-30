4108789a1c0386487efe10cd3bce11796cce4be9
Linda Kung <lindakung417@gmail.com>
2015-03-30 16:19:32 -0400

    You can view your code.

--- browser/js/about/userCode.js
+++ browser/js/about/userCode.js
@@ -10,15 +10,32 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('UserCodeCtrl', function($scope, AuthService, UsersFactory) {
+app.controller('UserCodeCtrl', function($scope, AuthService, UsersFactory, TestFactory) {
 	AuthService.getLoggedInUser().then(function(archivedUser) {
 		UsersFactory.getUser(archivedUser._id).then(function(user) {
 			$scope.user = user;
-			console.log('this page user', $scope.user);
 
 			$scope.userExercise = [];
 			$scope.user.exercises.forEach(function(exercise) {
 				$scope.userExercise.push(exercise);
+			})
+			console.log('userExercise', $scope.userExercise);
+
+			TestFactory.getExercises().then(function(data) {
+				$scope.userExercises = [];
+				var exercises = _.pluck(data, "_id");
+
+				$scope.user.uniqueChallenges.forEach(function(exercise) {
+					var userExercises = {};
+					var i = exercises.indexOf(exercise._id);
+					if(i === -1) {
+						console.log('No exercises completed');
+					} else {
+						userExercises = data[i];
+						$scope.userExercises.push(userExercises);
+						console.log('ANYTHING?', $scope.userExercises);
+					}
+				})
 			})
 
 		})
