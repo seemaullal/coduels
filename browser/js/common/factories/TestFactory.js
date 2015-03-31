466677f79aa7df86bc1b96850b9349070f90c638
iloughman <iloughman@gmail.com>
2015-03-31 12:49:45 -0400

    Update

---
+++ browser/js/common/factories/TestFactory.js
@@ -0,0 +1,19 @@
+
+'use strict';
+app.factory('TestFactory', function ($http){
+	var factory = {};
+
+	factory.submitTest = function (exercise) {
+		return $http.post('/api/exercises', exercise).then(function (response){
+			return response;
+		});
+	};
+
+	factory.getExercises = function () {
+		return $http.get('/api/exercises').then(function (response){
+			return response.data;
+		});
+	};
+
+	return factory;
+});
