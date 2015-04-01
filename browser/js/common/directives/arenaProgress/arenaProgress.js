18e5dd4b49a083e43b801d00f28710411a24eb59
iloughman <iloughman@gmail.com>
2015-04-01 11:30:51 -0400

    Nothing significant

--- browser/js/common/directives/arenaProgress/arenaProgress.js
+++ browser/js/common/directives/arenaProgress/arenaProgress.js
@@ -3,13 +3,12 @@ app.directive('arenaProgress', function (){
 	return {
 		restrict: 'E',
 		templateUrl: 'js/common/directives/arenaProgress/arenaProgress.html',
+		scope: {
+			user: '=user'
+		}
 		link: function (scope){
-			if (!scope.passedTests) {scope.passedTests = 0}
-			else if (scope.passedTests !== scope.user.passed) {
-				console.log("passedTests", scope.passedTests);
-				console.log("scope.user.passed", scope.user.passed);
-				scope.passedTests = scope.user.passed;
-			};
+			scope.passedTests = user.passed;
+			console.log("passed tests", scope.passedTests);
 		}
 	};
 });
