e8c7a196a77c794f3b6b7f50927d37292024f0bc
Seema <seemaullal@gmail.com>
2015-04-04 19:19:03 -0400

    Tests are displaying

--- server/io/index.js
+++ server/io/index.js
@@ -4,23 +4,23 @@ var io = null;
 
 module.exports = function (server) {
 
-    if (io) return io;
+    // if (io) return io;
 
-    io = socketio(server);
+    // io = socketio(server);
 
-    io.on('connection', function (socket) {
-        socket.on('userCode', function (code){
-        	console.log(code);
-        	socket.broadcast.emit('theCode', code);
-        });
+    // io.on('connection', function (socket) {
+    //     socket.on('userCode', function (code){
+    //     	console.log(code);
+    //     	socket.broadcast.emit('theCode', code);
+    //     });
 
-        socket.on('failures', function (failures){
-            console.log('failures', failures);
-            socket.broadcast.emit('theFailures', failures);
-        });
+    //     socket.on('failures', function (failures){
+    //         console.log('failures', failures);
+    //         socket.broadcast.emit('theFailures', failures);
+    //     });
 
-        socket.on('testsFailed', function (arrFromIframe){
-        	socket.broadcast.emit('failedTests', arrFromIframe);
-        });
-    });
+    //     socket.on('testsFailed', function (arrFromIframe){
+    //     	socket.broadcast.emit('failedTests', arrFromIframe);
+    //     });
+    // });
 };
