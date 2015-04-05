b7c0a9e49058479969ac80e6f81f4fc723654472
Seema <seemaullal@gmail.com>
2015-04-05 00:27:17 -0400

    Coduels

--- browser/js/common/directives/github-logo/github-logo.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-app.directive('githubLogo', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/github-logo/github-logo.html'
-    };
-});
--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,13 +7,6 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function (scope) {
 
-            // scope.items = [
-            //     { label: 'Home', state: 'home' },
-            //     { label: 'About', state: 'about' },
-            //     { label: 'Tutorial', state: 'tutorial' },
-            //     { label: 'Members Only', state: 'membersOnly', auth: true }
-            // ];
-
             scope.user = null;
 
             scope.isLoggedIn = function () {
