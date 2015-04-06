05734071ca7cf25cf75bc55836538fc280563c1a
Linda Kung <lindakung417@gmail.com>
2015-04-05 22:37:34 -0400

    Minor changes. Need to display user code

--- browser/js/viewcode/view-code.js
+++ browser/js/viewcode/view-code.js
@@ -26,12 +26,17 @@ app.controller('ViewCodeCtrl', function($scope, AuthService, UsersFactory, $stat
 
 					$scope.viewUser.exercises.forEach(function(viewUserExercise) {
 						if(exercise.name === viewUserExercise.exerciseID.name) {
+							console.log('view user code', viewUserExercise.code);
 							exercise.viewUserCode.push(viewUserExercise.code);
+
 							
-							exercise.viewUserCode = _.compact(exercise.viewUserCode);//
+							exercise.viewUserCode = _.compact(exercise.viewUserCode);
+
+							
+							// exercise.viewUserCode[exercise.viewUserCode.length - 1];//
 						}
 					})
-					console.log('shared user code', $scope.sharedCode);
+					console.log('shared user code', $scope.sharedCode.testCode);
 					
 				})
 				
